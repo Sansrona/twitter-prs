c35f1f9820309b85fb9a8ee75b2c148e4acd5f1e
Sansrona <kingJaM00@yandex.ru>
2021-11-13 19:09:24 +0600

    add: user registration

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo SITE_NAME ?></title>
+    <title><?php echo get_page_title($title) ?></title>
     <link rel="stylesheet" href="<?php echo get_url('css/style.css') ?>">
 </head>
 
--- user_posts.php
+++ user_posts.php
@@ -5,6 +5,7 @@ if(isset($_GET["id"]) && !empty($_GET["id"])){
     $id = $_GET["id"];
 }
 $posts = get_posts($id);
+$title= 'Твиты пользователя @'.$posts[0]['login'];
 include_once "includes/header.php";
 include_once "includes/posts.php";  
 include_once "includes/footer.php";
